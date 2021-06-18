340d47fe948f475e6081e6824d453a347bce83b5
unknown <sam.ferebee@gmail.com>
2021-06-18 18:33:58 -0400

    init

---
+++ Assets/Enemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    float speed = 8;
+    public Transform targetTransform;
+    void Start()
+    {
+        transform.position = new Vector3(5,.51f,5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+    }
+
+    void HandleMovement(){
+        //1. calc direction between enemy and target 2) take our pos - their pos to get displacement 3) normalize vector to get direction
+        Vector3 displacementFromTarget = targetTransform.position - transform.position;
+        Vector3 directionToTarget = displacementFromTarget.normalized;
+        Vector3 velocity = directionToTarget * speed;
+        if(displacementFromTarget.magnitude > 1.5f){
+            transform.Translate(velocity * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float speed = 10;
+    void Start()
+    {
+        transform.position = new Vector3(0,.51f,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       HandleMovement();
+    }
+
+void HandleMovement(){
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 direction = input.normalized;
+        Vector3 velocity = direction * speed;
+        Vector3 moveAmount = velocity * Time.deltaTime;
+        transform.Translate(moveAmount);
+    }
+}
